9412e2d4e888ec03be6c2bc0150da47afe210f17
soulmachine <soulmachine@gmail.com>
2021-01-04 07:42:47 -0800

    Empty project created by npx @docusaurus/init@latest init carbonbot.io classic

---
+++ docusaurus.config.js
@@ -0,0 +1,111 @@
+module.exports = {
+  title: "CarbonBot",
+  tagline: "Cryptocurrency Data Provider",
+  url: "https://www.carbonbot.io",
+  baseUrl: "/",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "soulmachine", // Usually your GitHub org/user name.
+  projectName: "carbonbot.io", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "CarbonBot",
+      logo: {
+        alt: "CarbonBot",
+        src: "img/logo.jpg",
+      },
+      items: [
+        {
+          to: "docs/",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "left",
+        },
+        { to: "blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/soulmachine/crypto-crawler-rs",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    googleAnalytics: {
+      trackingID: "UA-7583537-13",
+    },
+    gtag: {
+      trackingID: "UA-7583537-13",
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Style Guide",
+              to: "docs/",
+            },
+            {
+              label: "Second Doc",
+              to: "docs/doc2/",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+            },
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/docusaurus",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/facebook/docusaurus",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} CarbonBot. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
